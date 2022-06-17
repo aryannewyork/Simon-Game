bf1eaf82339cec9eaddb9e9fe22de51d54707192
Aryan Shukla <2020ucs0077@iitjammu.ac.in>
2022-06-17 15:58:05 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,81 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+
+$(document).keypress(function (){
+    if(!started){
+        $("#level-title").text("Level "+level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        console.log("wrong");
+        playSound("wrong");
+        $(document.body).addClass("game-over");
+        setTimeout(function(){
+            $(document.body).removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        stratOver();
+
+    }
+}
+
+
+function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level "+level);
+
+    var randomNum = Math.floor(Math.random()*4);
+    var randomChosenColor = buttonColors[randomNum];
+    gamePattern.push(randomChosenColor); 
+
+    $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    // $("#"+randomChosenColor).fadeTo(100, 0.3, function() { $(this).fadeTo(500, 1.0); });
+    playSound(randomChosenColor);
+
+}
+
+function playSound(randomChosenColor){
+    var newSound = new Audio("sounds/"+randomChosenColor+".mp3");
+    newSound.play();
+}
+
+function animatePress(currentColor) {
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function stratOver(){
+    level = 0;
+    started = false;
+    gamePattern = [];
+}
